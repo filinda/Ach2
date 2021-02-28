db8c5502e4b8211a39062d61b0fefa7bdfce7684
filinda <42602257+filinda@users.noreply.github.com>
2021-02-28 18:22:36 +0300

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,12 +1,50 @@
 const { createPool } = require("mysql");
+var port = process.env.mysqlbdport;
+var host = process.env.mysqlbdip+"";
+var user = process.env.mysqlbdusr+"";
+var password = process.env.mysqlbdpsw+"";
+var database = process.env.mysqlbdschema+"";
+var connectionLimit = process.env.mysqlbdconnectLimit;
+
+if (port==undefined) {
+	console.log('env variable mysqlbdport not found, port is default');
+	port = 3306;
+}
+
+if (host=='undefined') {
+	console.log('env variable mysqlbdip not found, host is default');
+	host='localhost';
+}
+
+
+if (user=='undefined') {
+	console.log('env variable mysqlbdusr not found, user is default');
+	user = 'user1';
+}
+
+if (password=='undefined') {
+	console.log('env variable mysqlbdpsw not found, password is default');
+	password = 'Qwer123!';
+}
+
+if (database=='undefined') {
+	console.log('env variable mysqlbdschema not found, database is default');
+	database = 'ach2';
+}
+
+if (connectionLimit==undefined) {
+	console.log('env variable mysqlbdconnectLimit not found, connectionLimit is default');
+	connectionLimit = 10;
+}
+
 
 const pool = createPool({
-    port:3306,
-    host:"127.0.0.1",
-    user:"user",
-    password:"user",
-    database:"ach2",
-    connectionLimit: 10
+	port:port,
+	host:host,
+	user:user,
+	password:password,
+	database:database,
+	connectionLimit: connectionLimit
 });
 
 module.exports = pool;
